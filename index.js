712e8aa6719af0c533c24422b4db94e68b1a957c
FRANCIS YELFAARI <94383215+amazingbookswa@users.noreply.github.com>
2022-04-26 07:00:54 -0700

    admin role

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-const express =require("express")
+const express = require("express")
 const morgan = require("morgan")
 require("dotenv").config()
-const connectDB = require("./config/connectDB")
-const foodRoute = require("./routes/foodRoute")
+const connectDB =require("./config/connectDB")
+const foodRoute =require("./routes/foodRoute")
+const userRoute = require ("./routes/userRoute")
 
-
+const app = express()
 
 connectDB()
 
-const app = express()
-//middlewares
+//middleware
 app.use(express.json())
 app.use(morgan("dev"))
-app.use(foodRoute)
+app.use("/api/foods", foodRoute)
+app.use("/api/users", userRoute)
 
-
-
-
-const PORT = process.env.PORT||9000
-
+const PORT =process.env.PORT||9000
 //Home route
 app.get("/", (req, res)=>{
-    res.json("welcome to my Menu API")
+    res.json("Welcome to my Menu API")
 })
 
+
+
 app.listen(PORT, ()=>{
-    console.log("Server is UP!!!")
+    console.log("Running")
 })
--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,10 +1,9 @@
 const {Router} = require("express")
-const { createFood, getMenu } = require("../controllers/foodController")
+const { createFood, getMenu } = require("../controllers/foodControllers")
+const { protect, admin}=require("../middlewares/authMiddleware")
 
 const router =Router()
 
-router.route("/foods").post(createFood).get(getMenu)
-
-
+router.route("/").post(protect, admin, createFood).get(protect, getMenu)
 
 module.exports=router
